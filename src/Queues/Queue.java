65aed0521e6f295b3914db692215c0e5504642cc
Paul C <pcutter@cnm.edu>
2021-05-06 11:03:01 -0600

    Updated to the remove method

--- src/Queues/Queue.java
+++ src/Queues/Queue.java
@@ -1,19 +1,40 @@
 package Queues;
 
-
 import LinkedLists.Node;
 
 public class Queue {
-
-  Node head = null;
-  Node tail = null;
-
+  public Node head = null;
+  public Node tail = null;
   public boolean isEmpty(){
     return head == null;
   }
-
   public void add(Object value) {
+    Node node = new Node(value);
 
+    if (head == null) {
+      head = node;
+      tail = node;
+    } else {
+      Node tmp = tail;
+      tmp.next = node;
+      tail = tmp.next;
+    }
   }
+
+  public Object peak() {
+    return head.data;
+  }
+
+  public void remove() {
+
+    if (head == null) {
+      throw new NullPointerException();
+    } else if (head == tail) {
+      head = null;
+      tail = null;
+    }
+    head = head.next;
+  }
+
 
 }
--- testQueue/QueueTest.java
+++ testQueue/QueueTest.java
@@ -4,21 +4,58 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class QueueTest {
-
   @Test
   public void testIsEmpty() {
     Queue queue = new Queue();
-
     assertTrue(queue.isEmpty());
   }
-
   @Test
   public void testAddToTail() {
     Queue queue = new Queue();
     queue.add(1);
-
-    assertEquals(1, queue.head.data, "Adding 1 to a new queue should make 1 the head & tail");
+    assertEquals(1, queue.tail.data, "Adding 1 to a new queue should make 1 the tail");
+  }
+  @Test
+  public void testAddToEmptyQueue () {
+    Queue queue = new Queue();
+    queue.add(1);
+    assertEquals(1, queue.head.data, "Adding 1 to a new queue should make 1 the head");
+  }
+  @Test
+  public void testAddToNonEmptyQueue () {
+    Queue queue = new Queue();
+    queue.add(1);
+    queue.add("second");
+    queue.add(true);
+    Object expected = "second";
+    Object actual = queue.head.next.data;
+    assertEquals(1, queue.head.data, "the head should be the first item added and remain the same");
+    assertEquals(expected, actual, "the body value is as it should be");
+    assertEquals(true, queue.tail.data, "the tail should be the last item added");
   }
 
+  @Test
+  public void testPeak() {
+    Queue queue = new Queue();
+    queue.add(1);
+    queue.add("second");
+    queue.add(true);
+    Object expected = 1;
+
+    assertEquals(expected, queue.peak(), "Peak should return head without removing it");
+    assertEquals(expected, queue.head.data, "Making sure the head hasn't been removed");
+  }
+
+  @Test
+  public void testRemove() {
+
+    Queue queue = new Queue();
+    queue.add(1);
+    queue.add("second");
+    queue.add(true);
+    queue.remove();
+
+    assertEquals("second", queue.head.data, "Second item in queue should be the new head");
+  }
 
 }
